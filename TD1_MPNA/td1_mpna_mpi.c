673d49a4c05eb3b8cd868a94a3884eb9b6a3a727
Maxime Kermarquer <maxime.kermarquer@gmail.com>
2016-12-10 16:51:50 +0100

    Modification de la fonction produit_scalaire_parralle

--- TD1_MPNA/td1_mpna_mpi.c
+++ TD1_MPNA/td1_mpna_mpi.c
@@ -10,6 +10,7 @@
 // 	int debut;
 // 	int fin;
 // };
+// struct decoupage_mpi decoupage = (struct decoupage_mpi) malloc (sizeof(struct decoupage_mpi));
 
 void affiche(double* tab, int N, int width){
 	int i;
@@ -41,19 +42,17 @@ double produit_scalaire (double* vecteur1, double* vecteur2, int taille_vecteur)
 }
 
 
-double produit_scalaire_parralle (double* vecteur1, double* vecteur2, int taille_vecteur, int debut, int fin, int rank){
+double produit_scalaire_parralle (double* vecteur1, double* vecteur2, int taille_vecteur){
 	double produit_scalaire;
 	int i;
 	produit_scalaire = 0;
 
-	printf("Process %d bien rentré dans fonction \n", rank);
-
-	for(i=debut; i<fin; i++){
+	//Chaque processus va faire une partie des calculs
+	for(i=0; i<taille_vecteur; i++){
 		produit_scalaire += vecteur1[i] * vecteur2[i];
-		printf("Process %d : %d\n", rank, i);
 	}
 
-	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Allreduce (&produit_scalaire, &produit_scalaire, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
 	return produit_scalaire;
 }
@@ -73,7 +72,7 @@ double* produit_matrice_vecteur_parrallele (double* matrice, double* vecteur, in
 }
 
 int main (int argc, char** argv){
-
+     
 	if(argc < 3){
 		fprintf(stderr, "Erreur argument : Usage %s <int : nbligne> <int : nbcol>\n",argv[0]);
 		exit(-1);
@@ -81,9 +80,11 @@ int main (int argc, char** argv){
 
 	int nb_ligne, nb_col;
 	double* vecteur1;
+	double* vecteur1_complet;
 	double* vecteur2;
-	double*  matrice;
-	double*  matrice_vecteur;
+	double* vecteur2_complet;
+	double* matrice;
+	// double*  matrice_vecteur;
 	int i,j;
 
 	struct timeval debut_calcul, fin_calcul, duree_calcul;
@@ -97,10 +98,15 @@ int main (int argc, char** argv){
 	nb_col = atoi(argv[2]);
 
 
-	if(rank == 0){
-		printf("--- Debut du calcul des decoupage --- \n");
+	//Le processus 0 va faire les calculs en sequentiel pour pouvoir comparer ensuite les
+	//resultats sequentiels et paralleles.
+	if(rank==0){
+		vecteur1_complet = (double*) malloc(nb_ligne*sizeof(double));
+		vecteur2_complet = (double*) malloc(nb_ligne*sizeof(double));
 	}
-	MPI_Barrier(MPI_COMM_WORLD);
+
+
+	//Calcul des decoupages des données 
 	int debut, fin;
 	int quotient, reste;
 	quotient = nb_ligne / size;
@@ -116,39 +122,44 @@ int main (int argc, char** argv){
 	}
 
 	int nbligne_process = fin-debut+1;
-	printf("\tRang %d :  nombre de lignes gérés %d\n",rank, nbligne_process);
-	
+
+
+	//Allocation par tous les processus
 	vecteur1 = (double*) malloc(nbligne_process*sizeof(double));
 	vecteur2 = (double*) malloc(nbligne_process*sizeof(double));
 	matrice  = (double*) malloc(nbligne_process*nb_col*sizeof(double));
-	// printf("Nombres de mega-octets alloués %d \n", nb_ligne*nb_col*sizeof(double)/1000000);
 
-	//Initialisation
+	//Initialisation par tous les processus
 	for(i=0; i<nbligne_process; i++){
 		vecteur1[i] = 2; //(double) (rand()%100);
-		vecteur2[i] = 2; //(double) (rand()%100);
+		vecteur2[i] = 3; //(double) (rand()%100);
 		for(j=0; j<nb_col; j++){
 			matrice[j + i*nb_col] = (double) i;
 		}
 	}
 
-	// printf("Avant Send\n");
-	// if(rank == 0){
-	// 	for(i=0; i<size; i++){
-	// 		MPI_Send(&vecteur1, 1, MPI_DOUBLE, i, 1000, MPI_COMM_WORLD);
-	// 	}
-	// }
-	// else{
-	// 	MPI_Recv(&vecteur1, 1, MPI_DOUBLE, 0, 1000, MPI_COMM_WORLD, NULL);
-	// }
+	//Le processus 0 récupère les données 
+	MPI_Gather(vecteur1, nbligne_process, MPI_DOUBLE, vecteur1_complet, nbligne_process, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Gather(vecteur2, nbligne_process, MPI_DOUBLE, vecteur2_complet, nbligne_process, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+	//Affichage des données récupérées par le processus 0
+	if(rank == 0){
+		printf("Vecteur1 complet\n");
+		affiche(vecteur1_complet, nb_ligne, nb_ligne);
+		printf("Vecteur2 complet\n");
+		affiche(vecteur2_complet, nb_ligne, nb_ligne);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+
+
+	//Affichage des des données de chaque processus
 	// for(i=0; i<size; i++){
 	// 	if(i==rank){
 	// 		printf("Rang : %d, debut : %d, fin : %d \n",rank,debut,fin);
-	// 		printf("vecteur1\n");
-	// 		affiche(vecteur1, nb_ligne, nb_ligne);
-	// 		printf("vecteur2\n");
-	// 		affiche(vecteur2, nb_ligne, nb_ligne);		
+	// 		printf("\tvecteur1\n");
+	// 		affiche(vecteur1, nbligne_process , nbligne_process);
+	// 		printf("\tvecteur2\n");
+	// 		affiche(vecteur2, nbligne_process, nbligne_process);		
 	// 	}
 	// 	MPI_Barrier(MPI_COMM_WORLD);
 	// }
@@ -156,24 +167,24 @@ int main (int argc, char** argv){
 
 
 
-	// struct decoupage_mpi decoupage = (struct decoupage_mpi) malloc (sizeof(struct decoupage_mpi));
-
-
-
-	//---------------------------------------------------------------------------------------------//
+  	//---------------------------------------------------------------------------------------------//
 
 	// En parallele
 
-	// gettimeofday(&debut_calcul, NULL);
-	// for (int i = 0; i < 100; ++i)
-	// {
-	// 	produit_scalaire_parralle(vecteur1, vecteur2, nb_ligne, debut, fin);
-	// }
-	// gettimeofday(&fin_calcul, NULL);
-	// timersub(&fin_calcul, &debut_calcul, &duree_calcul);
-	// printf("Temps produit scalaire parallèle : %f s\n", (double) (duree_calcul.tv_sec) + (duree_calcul.tv_usec / 1000000.0));
+	int resultat;
+	int resultat_seq;
+	double t1, t2;
 
+	t1 = MPI_Wtime();
+	resultat = produit_scalaire_parralle(vecteur1, vecteur2, nbligne_process);
+	t2 = MPI_Wtime();
 
+	if(rank == 0){
+		resultat_seq = produit_scalaire(vecteur1_complet , vecteur2_complet, nb_ligne);
+		printf("\tResultat = %d\n",resultat);
+		printf("\tResultat sequentiel = %d\n",resultat_seq);
+		// printf("Temps produit scalaire parallèle : %f s\n", (double) (duree_calcul.tv_sec) + (duree_calcul.tv_usec / 1000000.0));
+	}
 	// if(rank ==0){
 	// 	printf("\nProduit scalaire vecteur1 vecteur2 : \n");
 	// }
@@ -197,6 +208,10 @@ int main (int argc, char** argv){
 	free(vecteur1);
 	free(vecteur2);
 	free(matrice);
+	if(rank == 0){
+		free(vecteur1_complet);
+		free(vecteur2_complet);
+	}
 
 	MPI_Finalize();
 
