4eca55463b3a35f151ade89373b5377948f156f9
akloul amira <amira.akloul@ens.uvsq.fr>
2016-12-10 17:41:21 +0100

    legeres modifications

---
+++ AMIRATION/MATVEC_mpi.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <sys/time.h>
+
+
+
+//AFFICHAGE
+void affiche(double* tab, int N, int width){
+	int i;
+	printf("(");
+	for(i=0; i<N; i++){
+		if((i+1)%width == 0){
+			printf("%f)\n",tab[i]);
+			if(i != (N-1)){
+				printf("(");
+			}
+		}
+		else{
+			printf("%f ",tab[i]);
+		}
+	}
+}
+
+//PRODUIT SCALAIRE SEQUENTIEL
+double produit_scalaire(double *vecteur1, double *vecteur2, int taille){
+
+int i;
+double resultat=0;
+
+for(i=0; i<taille; i++){
+	resultat += vecteur1[i]*vecteur2[i];
+}
+return resultat;
+}
+
+
+
+
+//PRODUIT MATRICE VECTEUR SEQUENTIEL
+double *produit_matrice_vecteur(double *matrice, double *vecteur1, int nb_lignes, int nb_colonnes){
+
+int i;
+double *resultat=(double *)malloc(nb_lignes*sizeof(double));
+
+for(i=0; i<nb_lignes; i++){
+	resultat[i]=produit_scalaire(matrice+(i*nb_colonnes),vecteur1, nb_colonnes);
+}
+return resultat;
+}
+
+
+
+//MAIN
+int main(int argc, char **argv){
+
+
+int i, j, nb_lignes, nb_colonnes;
+
+int size, rank, ideb, ifin, Q, R;
+
+struct timeval debut_calcul, fin_calcul, duree_calcul;
+
+
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD,&size);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+Q=nb_lignes/size;
+R=nb_lignes%size;
+
+
+	if (rank < R){
+		ideb = rank * (1+Q);
+		ifin = ideb + Q;
+	}
+	else{
+		ideb = R * (1+Q) + ((rank - R) * Q);
+		ifin = ideb + Q - 1;
+	}
+
+
+
+if(argc<3){
+	printf("erreur argument\n");
+	printf("exemple d'execution: ./executable <nb_lignes> <nb_colonnes>\n"); 
+	exit(1);
+}
+
+nb_lignes=atoi(argv[1]);
+nb_colonnes=atoi(argv[2]);
+
+if(rank==0){
+double *vecteur1_complet=(double *)malloc(nb_lignes*sizeof(double));
+double *vecteur2_complet=(double *)malloc(nb_lignes*sizeof(double));
+double *matrice_complete=(double *)malloc((nb_lignes*nb_colonnes)*sizeof(double));
+}
+
+
+
+//INITIALISATION DES VECTEURS ET DES MATRICES
+
+printf("Initialisation des données\n");
+
+for(i=0; i<nb_lignes; i++){
+	vecteur1_complet[i]=2;
+	vecteur2_complet[i]=2;
+
+	for(j=0; j<nb_colonnes; j++){
+	matrice_complet[j+(i*nb_colonnes)]=2;		
+	}
+}
+
+/*
+printf("vecteur1\n");
+affiche(vecteur1, nb_lignes, nb_lignes);
+
+printf("vecteur2\n");
+affiche(vecteur2, nb_lignes, nb_lignes);
+
+printf("matrice\n");
+affiche(matrice, nb_lignes*nb_colonnes, nb_colonnes);
+*/
+
+//COMPLEXITE EN TEMPS POUR LE PRODUIT SCALAIRE ET LE PRODUIT MATRICE VECTEUR EN SEQUENTIEL
+
+
+
+//COMPLEXITE EN ESPACE MEMOIRE 
+printf("La complexité en espace memoire : %d\n", (1+ (nb_lignes*3) + (nb_lignes*nb_colonnes)));
+
+
+free(vecteur1);
+free(vecteur2);
+free(matrice);
+
+exit(0);
+
+}
