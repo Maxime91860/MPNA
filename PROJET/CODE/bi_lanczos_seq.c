1268b8ea084165a30df93596f64fa40838e64232
Maxime Kermarquer <maxime.kermarquer@gmail.com>
2017-02-23 15:38:13 +0100

    Projet : Nettoyage + Commentaires du code

--- PROJET/CODE/bi_lanczos_seq.c
+++ PROJET/CODE/bi_lanczos_seq.c
@@ -11,8 +11,12 @@
 #include <math.h>
 #include <string.h>
 
+//Dans ce programme nos vecteurs sont stockés en ligne.
 
+
+//Fonction d'affichage d'un vecteur ou d'une matrice
 void affiche(double* tab, int N, int width){
+
 	int i;
 	printf("(");
 	for(i=0; i<N; i++){
@@ -28,7 +32,9 @@ void affiche(double* tab, int N, int width){
 	}
 }
 
+//Calcul de la norme 2 d'un vecteur
 double norme_vecteur (double* vecteur, int taille_vecteur){
+
 	double somme = 0;
 	double norme;
 	int i;
@@ -42,7 +48,9 @@ double norme_vecteur (double* vecteur, int taille_vecteur){
 	return norme;
 }
 
+//Calcul du produit scalaire de deux vecteurs
 double produit_scalaire (double* vecteur1, double* vecteur2, int taille_vecteur){
+
 	double produit_scalaire;
 	int i;
 	produit_scalaire = 0;
@@ -54,7 +62,9 @@ double produit_scalaire (double* vecteur1, double* vecteur2, int taille_vecteur)
 	return produit_scalaire;
 }
 
+//Calcul du produit matrice par vecteur
 double* produit_matrice_vecteur (double* matrice, double* vecteur, int nb_ligne, int nb_col){
+
 	double* resultat;
 	int i;
 
@@ -67,38 +77,43 @@ double* produit_matrice_vecteur (double* matrice, double* vecteur, int nb_ligne,
 	return resultat;
 }
 
-double* mul_vecteur_constante (double constante, double* vecteur, int n){
+//Calcul du produit d'un vecteur par un scalaire
+double* mul_vecteur_scalaire (double scalaire, double* vecteur, int n){
+
 	double* resultat;
 	int i;
 
 	resultat = (double*) malloc(n*sizeof(double));
 
 	for(i=0; i<n; i++){
-		resultat[i] = vecteur[i] * constante;
+		resultat[i] = vecteur[i] * scalaire;
 	}
 
 	return resultat;
 }
 
-void diff_vecteurs (double* vecteur1, double* vecteur2, double* resultat, int n){
-	int i;
+//Calcul de la division d'un vecteur par un scalaire
+void div_vecteur_scalaire (double* vecteur, double scalaire, int n){
 
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		vecteur[i] = vecteur[i] / scalaire;
+	}
+}
+
+//Calcul de la différence entre deux vecteurs
+void diff_vecteurs (double* vecteur1, double* vecteur2, double* resultat, int n){
+
+	int i;
 	for(i=0; i<n; i++){
 		resultat[i] = vecteur1[i] - vecteur2[i];
 	}
 }
 
-void vecteur_div_constante(double* vecteur, double constante, int n){
-
-	int i;
-	for (i = 0; i < n; i++)
-	{
-		vecteur[i] = vecteur[i] / constante;
-	}
-}
-
-//Transposée d'une matrice carrée
+//Calcul de la transposée d'une matrice carrée
 double* transposee (double* A, int n){
+
 	double * At = (double *) malloc (n*n*sizeof(double));
 	int i, j; 
 	for(i=0; i<n; i++){
@@ -109,6 +124,31 @@ double* transposee (double* A, int n){
 	return At;
 }
 
+//Calcul du produit matriciel de la matrice1 (n1*m1) par matrice2 (n2*m2) (à valider)
+double* produit_matrice_matrice (double* matrice1, double* matrice2, int n1, int m1, int n2, int m2){
+
+	if(m1 != n2){
+		fprintf(stderr, "Erreur fonctions produit_matrice_matrice:\n--> m1 != n2\n");
+		return NULL;
+	}
+
+	int i, j;
+	// double* tmat2 = transposee(matrice2, n2);
+	double* resultat = (double *) malloc (n1*m2*sizeof(double));
+	for(i=0; i<n1; i++){
+		for(j=0; j<m2; j++){
+			resultat [i+ j*n1] = produit_scalaire(matrice1+(i*m1), matrice2+(j*m2), n2);
+		}
+	}
+	return resultat;
+}
+
+//Méthode de projection biorthogonale de Lanczos
+//On va creer :
+//	- deux bases V et W
+//	- la projection de la matrice A, dans T
+// V sera la base pour A
+// W sera la base pour la transposée de A, A^t
 void bi_lanczos(double* A, int m, int n){
 
 	double alpha;
@@ -117,10 +157,9 @@ void bi_lanczos(double* A, int m, int n){
 
 	FILE* log = fopen("log.txt","a");
 
-
 	//Vecteur initiaux
 	double* v = (double *) malloc ((m+2)*n*sizeof(double));
-	double* w = (double *) malloc ((m+2)* n*sizeof(double));
+	double* w = (double *) malloc ((m+2)*n*sizeof(double));
 	double* t = (double *) malloc (m*m*sizeof(double));
 	double* produit_A_v;
 	double* produit_At_w;
@@ -152,8 +191,13 @@ void bi_lanczos(double* A, int m, int n){
 		exit(-1);
 	}
 
+
+	/*---------------------------------------------------*/
+	/*--- PROCEDURE DE BIORTHOGONALISATION DE LANCZOS ---*/
+	/*---------------------------------------------------*/ 
 	int j;
 	for(j=1; j <= m; j++){
+		//Calcul des produits matrice * vecteur
 		produit_A_v = produit_matrice_vecteur (A, v+(j*n), n, n);
 		produit_At_w = produit_matrice_vecteur (At, w+(j*n), n, n);
 
@@ -165,35 +209,36 @@ void bi_lanczos(double* A, int m, int n){
 		double* res2;
 		double* res3 = (double *) malloc (n*sizeof(double));
 
-		//Calcul de v_j+1
-		res1 = mul_vecteur_constante(alpha, v+(j*n), n);
-		res2 = mul_vecteur_constante(beta, v+((j-1)*n), n);
+		//Calcul de v_(j+1)
+		res1 = mul_vecteur_scalaire(alpha, v+(j*n), n);
+		res2 = mul_vecteur_scalaire(beta, v+((j-1)*n), n);
 		diff_vecteurs(res1, res2, res3, n);
 		diff_vecteurs(produit_A_v, res3, v+((j+1)*n), n);
 
-		//Calcul de w_j+1
-		res1 = mul_vecteur_constante(alpha, w+(j*n), n);
-		res2 = mul_vecteur_constante(delta, w+((j-1)*n), n);
+		//Calcul de w_(j+1)
+		res1 = mul_vecteur_scalaire(alpha, w+(j*n), n);
+		res2 = mul_vecteur_scalaire(delta, w+((j-1)*n), n);
 		diff_vecteurs(res1, res2, res3, n);
 		diff_vecteurs(produit_At_w, res3, w+((j+1)*n), n);
 
 		delta = sqrt(abs(produit_scalaire(v+((j+1)*n), w+((j+1)*n), n)));
 		if(delta == 0){
-			fprintf(stderr, "Erreur numérique du calcul de delta#%d.\n",j);
+			fprintf(stderr, "Erreur numérique du calcul de delta à l'itération #%d.\n",j);
 			exit(-1);
 		}
 
 		beta = produit_scalaire (v+((j+1)*n), w+((j+1)*n), n) / delta;
 
+		//MaJ de w_(j+1)
+		div_vecteur_scalaire (w+((j+1)*n), beta, n);
 
-		vecteur_div_constante(v+((j+1)*n), beta, n);
-		vecteur_div_constante(w+((j+1)*n), delta, n);
+		//MaJ de v_(j+1)
+		div_vecteur_scalaire (v+((j+1)*n), delta, n);
 
 		//Remplissage de T
-		printf("alpha = %g\n",alpha);
 		t[j-1+(j-1)*m] = alpha;
 		if(j != m){
-			t[j-1+(j)*m] = beta;
+			t[j-1+(j)*m] = beta; 
 			t[j+(j-1)*m] = delta;
 		}
 	}
@@ -204,10 +249,11 @@ void bi_lanczos(double* A, int m, int n){
 	affiche(w, n*(m+1), n);
 	printf("T =\n");
 	affiche(transposee(t, m), m*m, m);
-
 }
 
+//Fonction de test des différentes fonctions implémentées
 void test(){
+
 	int n = 10;
 	double* vecteur1 = (double *) malloc (n*sizeof(double));
 	double* vecteur2 = (double *) malloc (n*sizeof(double));
@@ -215,9 +261,9 @@ void test(){
 
 	int i;
 	for(i=0; i<n; i++){
-		vecteur1[i] = i;
-		vecteur2[i] = 2*i;
-		vecteur3[i] = 3*i;
+	       vecteur1[i] = i;
+	       vecteur2[i] = 2*i;
+	       vecteur3[i] = 3*i;
 	}
 
 
@@ -229,7 +275,7 @@ void test(){
 	affiche(vecteur3, n, n);
 
 	//Test vecteur * scalaire
-	double* result = mul_vecteur_constante(4, vecteur1, n);
+	double* result = mul_vecteur_scalaire(4, vecteur1, n);
 	printf("--- TEST vect*scal ---\n  Vecteur1 * 4 =\n");
 	affiche(result, n, n);
 
@@ -238,24 +284,59 @@ void test(){
 	printf("--- TEST vect-vect ---\n  Vecteur1 - Vecteur1 =\n");
 	affiche(result, n, n);
 
-	//Test vecteur * scalaire
-	vecteur_div_constante(vecteur3, 3, n);
+	//Test vecteur / scalaire
+	div_vecteur_scalaire(vecteur3, 3, n);
+
 	printf("--- TEST vect/scal ---\n  Vecteur3 / 3 =\n");
 	affiche(vecteur3, n, n);
 
+	double * matrice1 = (double *) malloc (n*n*sizeof(double));
+	double * matrice2 = (double *) malloc (n*n*sizeof(double));
+
+	int j;
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			if(j==i){
+				matrice1[i + j*n] = rand()%3 + 0.;
+			}
+			else{
+				matrice1[i + j*n] = 0;
+			}
+			matrice2[i + j*n] = 2;
+		}
+	}
+	printf("matrice1 : \n");
+	affiche(matrice1, n*n, n);
+	printf("matrice2 : \n");
+	affiche(matrice2, n*n, n);
+	double* resultat = produit_matrice_matrice(matrice1, matrice2, n, n, n, n);
+	printf("resultat : \n");
+	affiche(resultat, n*n, n);
+
+	free(vecteur1);
+	free(vecteur2);
+	free(vecteur3);
+	free(matrice1);
+	free(matrice2);
+	free(resultat);
 }
 
-int main(int argc, char const *argv[])
-{
+
+int main(int argc, char const *argv[]){
 
 	if(argc < 3){
-		fprintf(stderr, "Erreur :\nUsage : %s <taille_matrice> <taille_ss_espace_krylov>\n", argv[0]);
+		fprintf(stderr, "Erreur arguments:\n\tUsage : %s <taille_matrice> <taille_ss_espace_krylov>\n", argv[0]);
 		exit(-1);
 	}
 
-	
 	int taille_ss_espace_krylov = atoi(argv[2]);
 	int taille_matrice = atoi(argv[1]);
+
+	if (taille_ss_espace_krylov > taille_matrice){
+		fprintf(stderr, "Erreur arguments:\n\tUsage : La <taille_ss_espace_krylov>  doit être inférieur ou égale à la <taille_matrice>\n");
+		exit(-1);
+	}
+
 	double* A = (double *) malloc (taille_matrice*taille_matrice*sizeof(double));
 
 	int i;
@@ -273,9 +354,9 @@ int main(int argc, char const *argv[])
 
 	
 
-	// test();
+	test();
 
-	bi_lanczos(A,taille_ss_espace_krylov, taille_matrice);
+	// bi_lanczos(A,taille_ss_espace_krylov, taille_matrice);
 
 	return 0;
 }
